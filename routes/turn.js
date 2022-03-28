8c9a9732a3f923935f88494758dcc3460bf038b8
NicolasRequejo <nicolasrequejo.96@gmail.com>
2022-03-28 12:46:40 -0300

    details

--- routes/turn.js
+++ routes/turn.js
@@ -1,73 +1,70 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const routerTurn = express.Router();
 
-
-const { User, Turn} = require("../models/index");
-const { isLogged, isOperator } = require('./utils');
+const { User, Turn } = require("../models/index");
+const { isLogged, isOperator } = require("./utils");
 
 //Turno (1) pending para un determinado Usuario
-routerTurn.get("/pending/:userId", isLogged ,(req,res) => {
-        const {userId} = req.params
-        User.findByPk({ where: {userId}})
-        .then(user => {
-            Turn.findOne({ where: {userId, state:"pending"}})
-            .then(turn => {
-                res.status(200).send(turn)
-            })
-        })
-        .catch(err => {
-            console.log(err)
-          })
-})
+routerTurn.get("/pending/:userId", isLogged, (req, res) => {
+  const { userId } = req.params;
+  User.findByPk({ where: { userId } })
+    .then((user) => {
+      Turn.findOne({ where: { userId, state: "pending" } }).then((turn) => {
+        res.status(200).send(turn);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //Alta de Turno
 
-routerTurn.post("/", isLogged,(req,res) => {
-    const turn = req.body;
-    Turn.create(turn)
-    .then(turn =>{
-        res.send(turn)
+routerTurn.post("/", isLogged, (req, res) => {
+  const turn = req.body;
+  Turn.create(turn)
+    .then((turn) => {
+      res.send(turn);
     })
-    .catch(err => {
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // ****************************  Rutas para Administrador **************************//
 //Actualización de state del turno
 
-routerTurn.put("/branch/admin/:id", isOperator, (req,res) => {
-    const { state } = req.body;
-    const { turnId } = req.params;
-    Turn.update(state, {where: { id: turnId }})
-    .then(turn =>{
-        res.send(turn)
+routerTurn.put("/branch/admin/:id", isOperator, (req, res) => {
+  const { state } = req.body;
+  const { turnId } = req.params;
+  Turn.update(state, { where: { id: turnId } })
+    .then((turn) => {
+      res.send(turn);
     })
-    .catch(err => {
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //Turnos por sucursal
 
-routerTurn.get("/branch/:branchId",isOperator, (req,res) => {
-    const { branchId } = req.params;
-    Turn.findAll({where: { branchId }})
-    .then(turns => {
-        res.status(200).send(turns)
-    })
-})
+routerTurn.get("/branch/:branchId", isOperator, (req, res) => {
+  const { branchId } = req.params;
+  Turn.findAll({ where: { branchId } }).then((turns) => {
+    res.status(200).send(turns);
+  });
+});
 
 //Retorna un turno en particular
-routerTurn.get("/:id",isOperator, (req,res) => {
-    const {id} = req.params
-    Turn.findOne({ where: {id}})
-    .then(turn => {
-        res.status(200).send(turn)
+routerTurn.get("/:id", isOperator, (req, res) => {
+  const { id } = req.params;
+  Turn.findOne({ where: { id } })
+    .then((turn) => {
+      res.status(200).send(turn);
     })
-    .catch(err => {
-        console.log(err)
-      })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-module.exports = router;
+module.exports = routerTurn;
