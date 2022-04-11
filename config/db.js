d996d189d5c48c443753b1578d582dfed5ff2fe0
Arkanyan <agustinrafa1995@gmail.com>
2022-04-11 17:22:53 -0300

    deploy

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize")
 const db = new Sequelize('cruce', null, null, {
-  host: 'localhost',
+  host: process.env.PSQL_HOST || 'localhost',
   dialect: 'postgres',
   logging: false,
 })
