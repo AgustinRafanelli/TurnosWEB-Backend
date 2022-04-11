7a4ea31d91e7b4a6b4943dbe19ef61e11b86f843
Arkanyan <agustinrafa1995@gmail.com>
2022-04-11 17:29:27 -0300

    database

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 const {Sequelize} = require("sequelize")
-const db = new Sequelize('cruce', null, null, {
-  host: process.env.PSQL_HOST || 'localhost',
-  dialect: 'postgres',
-  logging: false,
-})
+
+if (process.env.NODE_ENV === "production") {
+  var db = new Sequelize(process.env.DATABASE_URL);
+} else {
+  const db = new Sequelize('cruce', null, null, {
+    host: process.env.PSQL_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: false,
+  })
+}
 
 module.exports = db;
