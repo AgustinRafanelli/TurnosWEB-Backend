568bcf50d00c24bed1d4a727fc633e9ffcb8ff70
Arkanyan <agustinrafa1995@gmail.com>
2022-04-11 23:56:08 -0300

    update database 4 heroku

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const {Sequelize} = require("sequelize")
 let db
 if (process.env.NODE_ENV === "production") {
-  db = new Sequelize(process.env.DATABASE_URL)
+  db = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres', protocol: 'postgres', ssl: true })
 } else {
-  db = new Sequelize(
-  'cruce', 
+  db = new Sequelize('cruce', 
   null, 
   null, 
   {
     host: 'localhost',
     dialect: 'postgres',
     logging: false,
+    ssl: true
   })
 }
 
